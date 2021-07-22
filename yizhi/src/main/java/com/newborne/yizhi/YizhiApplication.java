98ff2a4b7964735eb2cbca72e42a909aa4f8bcf5
newborne <newborne@foxmail.com>
2021-07-22 22:26:38 +0800

    yizhi-demo

---
+++ yizhi/src/main/java/com/newborne/yizhi/YizhiApplication.java
@@ -0,0 +1,13 @@
+package com.newborne.yizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YizhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YizhiApplication.class, args);
+	}
+
+}
---
+++ yizhi/src/test/java/com/newborne/yizhi/YizhiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newborne.yizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YizhiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
